4d5a3297e945dd728f126ba0db45771ef84220df
KU Munasinghe <kasun.munasinghe@outlook.com>
2022-12-18 07:04:30 +0530

    delete

--- PMS/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PMS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
