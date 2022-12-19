3e4063f9e8af18590fc9dac648faa63c21c7efd7
KU Munasinghe <kasun.munasinghe@outlook.com>
2022-12-19 19:33:40 +0530

    pms_db

---
+++ pms.sql
@@ -0,0 +1,256 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 19, 2022 at 03:02 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+CREATE TABLE `company` (
+  `id` int(11) NOT NULL,
+  `company_name` text NOT NULL,
+  `company_tel` int(10) DEFAULT NULL,
+  `company_address` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `company`
+--
+
+INSERT INTO `company` (`id`, `company_name`, `company_tel`, `company_address`) VALUES
+(1, 'SPC', 112320350, 'SPC, Mehewara piyasa, 16th floor, No41, Kirulupana Road, Colombo 05, Sri Lanka.'),
+(8, 'wedfwdwdw', 0, 'fwfewfwww');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medicine`
+--
+
+CREATE TABLE `medicine` (
+  `medicine_id` int(11) NOT NULL,
+  `medicine_name` varchar(255) DEFAULT NULL,
+  `manufacture_id` int(11) DEFAULT NULL,
+  `unit_price` float DEFAULT NULL,
+  `medicine_type` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `medicine`
+--
+
+INSERT INTO `medicine` (`medicine_id`, `medicine_name`, `manufacture_id`, `unit_price`, `medicine_type`, `description`) VALUES
+(2, 'Paracetomol', 1, 3, 'pain killer', 'low pain killer');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `retail`
+--
+
+CREATE TABLE `retail` (
+  `retail_id` int(11) NOT NULL,
+  `retail_name` text NOT NULL,
+  `retail_type` varchar(255) DEFAULT NULL,
+  `retail_tel` int(20) DEFAULT NULL,
+  `retail_address` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `retail`
+--
+
+INSERT INTO `retail` (`retail_id`, `retail_name`, `retail_type`, `retail_tel`, `retail_address`) VALUES
+(1, 'Sirasa', 'Retail', 3411111, 'Kiwlawatta');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `sale_id` int(11) NOT NULL,
+  `sale_date` date NOT NULL DEFAULT current_timestamp(),
+  `sale_name` varchar(255) NOT NULL,
+  `sale_type` varchar(255) NOT NULL,
+  `sale_value` int(11) NOT NULL,
+  `sale_description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`sale_id`, `sale_date`, `sale_name`, `sale_type`, `sale_value`, `sale_description`) VALUES
+(1, '2022-12-18', 'test_sale', 'direct', 0, 'for paracetomol');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock`
+--
+
+CREATE TABLE `stock` (
+  `stock_id` int(11) NOT NULL,
+  `stockmedicine_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `capacity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `stock`
+--
+
+INSERT INTO `stock` (`stock_id`, `stockmedicine_id`, `quantity`, `capacity`) VALUES
+(1, 2, 100, 1000),
+(2, 2, 20, 500),
+(3, 2, 99, 100);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `mobile` int(10) DEFAULT NULL,
+  `address` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `mobile`, `address`) VALUES
+(1, 'Kasun', 'Udakara', 'test@test.com', '1', 0, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `medicine`
+--
+ALTER TABLE `medicine`
+  ADD PRIMARY KEY (`medicine_id`),
+  ADD KEY `manufacture_id` (`manufacture_id`);
+
+--
+-- Indexes for table `retail`
+--
+ALTER TABLE `retail`
+  ADD PRIMARY KEY (`retail_id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`sale_id`);
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`stock_id`),
+  ADD KEY `stockmedicine_id` (`stockmedicine_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `company`
+--
+ALTER TABLE `company`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `medicine`
+--
+ALTER TABLE `medicine`
+  MODIFY `medicine_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `retail`
+--
+ALTER TABLE `retail`
+  MODIFY `retail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `sale_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `stock_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `medicine`
+--
+ALTER TABLE `medicine`
+  ADD CONSTRAINT `medicine_ibfk_1` FOREIGN KEY (`manufacture_id`) REFERENCES `company` (`id`);
+
+--
+-- Constraints for table `stock`
+--
+ALTER TABLE `stock`
+  ADD CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`stockmedicine_id`) REFERENCES `medicine` (`medicine_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
