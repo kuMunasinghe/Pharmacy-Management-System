bffbb8f18d8eacece50397c0729e39017f5eda41
KU Munasinghe <kasun.munasinghe@outlook.com>
2022-12-19 15:40:05 +0530

    stable Comapny EN

--- Controllers/CompanyController.cs
+++ Controllers/CompanyController.cs
@@ -12,7 +12,7 @@ namespace PMS.Controllers
         public IActionResult Index()
         {
 
-            List<Company> company = new ();
+            List<Company> company = new();
             //MYSQL Connection
             using (MySqlConnection conn = new MySqlConnection("server=localhost;user=root;password='';port=3306;database=pms;"))
             {
@@ -23,7 +23,7 @@ namespace PMS.Controllers
                 while (reader.Read())
                 {
                     //GET DATA
-                    Company getcompany = new ();
+                    Company getcompany = new();
                     getcompany.Id = Convert.ToInt32(reader["id"]);
                     getcompany.Name = reader["company_name"].ToString();
                     getcompany.Contact = Convert.ToInt32(reader["company_tel"]);
@@ -70,6 +70,122 @@ namespace PMS.Controllers
                     }
 
                 }
+            }
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult CreateSubmit(Company company)
+        {
+            using (MySqlConnection conn = new MySqlConnection("server=localhost;user=root;password='';port=3306;database=pms;"))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = @"INSERT INTO company (id,company_name,company_tel, company_address) 
+                                     VALUES (NULL,@name,@tel,@add);";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@name", company.Name);
+                    cmd.Parameters.AddWithValue("@tel", company.Contact);
+                    cmd.Parameters.AddWithValue("@add", company.Address);
+
+                    // MySqlDataReader reader = cmd.ExecuteReader();
+                    try
+                    {
+
+                        var res = cmd.ExecuteNonQuery();
+                        conn.Close();
+
+                        if (res == 1)
+                        {
+                            TempData["Message"] = "Company Added Successfully !";
+                            return RedirectToAction("Index");
+                        }
+                        else
+                        {
+                            TempData["Message"] = "Error!";
+                            return RedirectToAction("Index");
+                        }
+
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                        TempData["Message"] = ex.Message+" !";
+                        return RedirectToAction("Index");
+
+                    }
+                }
+                catch (AggregateException)
+                {
+                    TempData["Message"] = "Database connection Error !";
+                    return RedirectToAction("Index");
+
+                }
+
+            }
+        }
+
+        public IActionResult Edit(int Id) 
+        {
+            TempData["ID"] = Id;
+            return View(); 
+        }
+
+        public IActionResult Save(Company com)
+        {
+            using (MySqlConnection conn = new MySqlConnection("server=localhost;user=root;password='';port=3306;database=pms;"))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = @"
+                                          UPDATE company SET company_tel = @tel, company_address = @add WHERE company.id = @id;
+                                       ";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("id", com.Id);
+                    cmd.Parameters.AddWithValue("tel", com.Contact);
+                    cmd.Parameters.AddWithValue("add", com.Address);
+
+                    // MySqlDataReader reader = cmd.ExecuteReader();
+                    try
+                    {
+
+                        var res = cmd.ExecuteNonQuery();
+                        conn.Close();
+
+                        if (res == 1)
+                        {
+                            TempData["Message"] = "Data Edited Successfully !";
+                            return RedirectToAction("Index");
+                        }
+                        else
+                        {
+                            TempData["Message"] = "Edit Failed !";
+                            return RedirectToAction("Index");
+                        }
+
+
+                    }
+                    catch (Exception ex)
+                    {
+                        TempData["Message"] = ex.Message;
+                        return RedirectToAction("Index");
+
+
+                    }
+                }
+                catch (AggregateException)
+                {
+                    TempData["Message"] = "Database Error !";
+                    return RedirectToAction("Index");
+
+                }
+
             }
         }
     }
