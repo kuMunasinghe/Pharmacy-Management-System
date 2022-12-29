7a21ccd6af0b1cf697efa3cef62096c776d622ce
KU Munasinghe <kasun.munasinghe@outlook.com>
2022-12-29 20:34:56 +0530

    pms db update

--- pms.sql
+++ pms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 19, 2022 at 05:21 PM
+-- Generation Time: Dec 29, 2022 at 04:03 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -207,7 +207,7 @@ ALTER TABLE `company`
 -- AUTO_INCREMENT for table `medicine`
 --
 ALTER TABLE `medicine`
-  MODIFY `medicine_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `medicine_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `retail`
